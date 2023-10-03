7878eb22da26becc2f8df56a2e21c8a0c43e323e
Layne Haber <layne.haber@gmail.com>
2023-10-03 17:19:47 -0600

    chore: add context lookup contracts

---
+++ test/utils/AddressLookup.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library AddressLookup {
+  function getConnext(uint256 _chainId) public pure returns (address) {
+    // Mainnets
+    if (_chainId == 1) {
+      return address(0x8898B472C54c31894e3B9bb83cEA802a5d0e63C6);
+    }
+
+    if (_chainId == 10) {
+      return address(0x8f7492DE823025b4CfaAB1D34c58963F2af5DEDA);
+    }
+
+    if (_chainId == 56) {
+      return address(0xCd401c10afa37d641d2F594852DA94C700e4F2CE);
+    }
+
+    if (_chainId == 100) {
+      return address(0x5bB83e95f63217CDa6aE3D181BA580Ef377D2109);
+    }
+
+    if (_chainId == 137) {
+      return address(0x11984dc4465481512eb5b777E44061C158CF2259);
+    }
+
+    if (_chainId == 42161) {
+      return address(0xEE9deC2712cCE65174B561151701Bf54b99C24C8);
+    }
+
+    // Testnets
+    if (_chainId == 5) {
+      return address(0xFCa08024A6D4bCc87275b1E4A1E22B71fAD7f649);
+    }
+
+    if (_chainId == 420) {
+      return address(0x5Ea1bb242326044699C3d81341c5f535d5Af1504);
+    }
+
+    if (_chainId == 80001) {
+      return address(0x2334937846Ab2A3FCE747b32587e1A1A2f6EEC5a);
+    }
+
+    if (_chainId == 421613) {
+      return address(0x2075c9E31f973bb53CAE5BAC36a8eeB4B082ADC2);
+    }
+
+    require(false, "!connext for chain");
+  }
+
+  function getConnextDao(uint256 _chainId) public pure returns (address) {
+    // Mainnets
+    if (_chainId == 1) {
+      return address(0x4d50a469fc788a3c0CdC8Fd67868877dCb246625);
+    }
+
+    if (_chainId == 10) {
+      return address(0x6eCeD04DdC5A7709d5877c963cED0288Fb1c7348);
+    }
+
+    if (_chainId == 56) {
+      return address(0x9435Ba7C661a0Fd477deED640491de8c100325A7);
+    }
+
+    if (_chainId == 100) {
+      return address(0x7616Bc6d0dee5E250BA5b3dDa6cbbB71786FB638);
+    }
+
+    if (_chainId == 137) {
+      return address(0x0970Adeb473609F91D03e9Bba85F49C445040cD7);
+    }
+
+    if (_chainId == 42161) {
+      return address(0x5C711DB90dEc0a5B81C626968DEa4187a7f9C1F2);
+    }
+
+    require(false, "!dao for chain");
+  }
+}
---
+++ test/utils/ChainLookup.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library ChainLookup {
+  function getRpcEnvName(uint256 _chainId) public pure returns (string memory) {
+    // Mainnets
+    if (_chainId == 1) {
+      return "MAINNET_RPC_URL";
+    }
+
+    if (_chainId == 10) {
+      return "OPTIMISM_RPC_URL";
+    }
+
+    if (_chainId == 56) {
+      return "BNB_RPC_URL";
+    }
+
+    if (_chainId == 100) {
+      return "GNOSIS_RPC_URL";
+    }
+
+    if (_chainId == 137) {
+      return "POLYGON_RPC_URL";
+    }
+
+    if (_chainId == 42161) {
+      return "ARBITRUM_ONE_RPC_URL";
+    }
+
+    // Testnets
+    if (_chainId == 5) {
+      return "GOERLI_RPC_URL";
+    }
+
+    if (_chainId == 420) {
+      return "OPTIMISM_GOERLI_RPC_URL";
+    }
+
+    if (_chainId == 80001) {
+      return "MUMBAI_RPC_URL";
+    }
+
+    if (_chainId == 421613) {
+      return "ARBITRUM_GOERLI_RPC_URL";
+    }
+
+    require(false, "!rpc env var for chain");
+  }
+
+  function getDomainId(uint256 _chainId) public pure returns (uint32) {
+    // Mainnets
+    if (_chainId == 1) {
+      return 6648936;
+    }
+
+    if (_chainId == 10) {
+      return 1869640809;
+    }
+
+    if (_chainId == 56) {
+      return 6450786;
+    }
+
+    if (_chainId == 100) {
+      return 6778479;
+    }
+
+    if (_chainId == 137) {
+      return 1886350457;
+    }
+
+    if (_chainId == 42161) {
+      return 1634886255;
+    }
+
+    // Testnets -- TODO
+
+    require(false, "!domain for chain");
+  }
+
+  function getNetworkName(uint256 _chainId) public pure returns (string memory) {
+    // Mainnets
+    if (_chainId == 1) {
+      return "mainnet";
+    }
+
+    if (_chainId == 10) {
+      return "optimism";
+    }
+
+    if (_chainId == 56) {
+      return "bnb";
+    }
+
+    if (_chainId == 100) {
+      return "xdai";
+    }
+
+    if (_chainId == 137) {
+      return "matic";
+    }
+
+    if (_chainId == 42161) {
+      return "arbitrum-one";
+    }
+
+    // Testnets
+    if (_chainId == 5) {
+      return "goerli";
+    }
+
+    if (_chainId == 420) {
+      return "optimism-goerli";
+    }
+
+    if (_chainId == 80001) {
+      return "mumbai";
+    }
+
+    if (_chainId == 421613) {
+      return "arbitrum-goerli";
+    }
+
+    require(false, "!network name for chain");
+  }
+}
